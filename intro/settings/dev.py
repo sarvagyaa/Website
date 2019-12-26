6986bccbf4a96a669533045d412d0fdfc070ed4d
sarvagyaa <abhimanyu.sarvagyam@gmail.com>
2019-12-26 16:49:20 +0800

    First commit

---
+++ intro/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'w3oo4lfqom80*h$&f*1u7d=ml4(e&(_tc*94h%oeh@zm%m7_d$'
+
+ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local_settings import *  # noqa
+except ImportError:
+    pass
